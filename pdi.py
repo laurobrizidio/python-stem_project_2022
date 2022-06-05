f4c4ce07ab200978c498bedfb74c81f27e243088
Lauro Brizidio <laurobrizidio@hotmail.com>
2022-06-05 17:47:06 -0400

    [PDI] Added grayscale logical

--- pdi.py
+++ pdi.py
@@ -4,13 +4,21 @@ def convertToBlackAndWhite(image : QImage):
     image_height = image.height()
     image_width = image.width()
 
-    POSITION_X = 500
-    POSITION_Y = 500
+    for h in range(image_height):
+        for w in range(image_width):
+            red = image.pixelColor(w,h).red()
+            green = image.pixelColor(w,h).green()
+            blue = image.pixelColor(w,h).blue()
 
-    print("Color RED: {} - GREEN: {} - BLUE: {}",
-    image.pixelColor(POSITION_X,POSITION_Y).red(),
-    image.pixelColor(POSITION_X,POSITION_Y).green(),
-    image.pixelColor(POSITION_X,POSITION_Y).blue())
+            pixel = convertRGBToBlackAndWhite(red,green,blue)
+            image.setPixelColor(w,h,QColor.fromRgb(pixel,pixel,pixel))
+
+    return image
+
+
+def convertToGrayScale(image : QImage):
+    image_height = image.height()
+    image_width = image.width()
 
     for h in range(image_height):
         for w in range(image_width):
@@ -18,11 +26,19 @@ def convertToBlackAndWhite(image : QImage):
             green = image.pixelColor(w,h).green()
             blue = image.pixelColor(w,h).blue()
 
-
-            pixel = convertRGBToBlackAndWhite(red,green,blue)
+            pixel = convertRGBToGray(red,green,blue)
             image.setPixelColor(w,h,QColor.fromRgb(pixel,pixel,pixel))
 
     return image
+
+# Considerando o modelo YUV para iluminância 
+    # Wr = 0.299
+    # Wg = 0.587
+    # Wb = 0.114
+
+def convertRGBToGray(r : int,g : int, b : int):
+    colorInt = (0.299 * r + 0.587 * g + 0.114 * b)/3
+    return colorInt
 
 
 def convertRGBToBlackAndWhite(r : int,g : int, b : int):
